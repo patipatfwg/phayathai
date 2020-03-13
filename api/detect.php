ef1c6c6ef3ac69009b192c749081fd0e4fece92d
Patipat Chanrungruang <patipat_cha@freewillsolutions.com>
2020-03-13 18:54:06 +0700

    1854

--- api/detect.php
+++ api/detect.php
@@ -5,15 +5,15 @@
 header('Content-Type: application/json');
 
 //Make sure that it is a POST request.
-if(strcasecmp($_SERVER['REQUEST_METHOD'], 'GET') != 0){
-    throw new Exception('Request method must be POST!');
-}
+// if(strcasecmp($_SERVER['REQUEST_METHOD'], 'GET') != 0){
+//     throw new Exception('Request method must be POST!');
+// }
  
 //Make sure that the content type of the POST request has been set to application/json
-$contentType = isset($_SERVER["CONTENT_TYPE"]) ? trim($_SERVER["CONTENT_TYPE"]) : '';
-if(strcasecmp($contentType, 'application/json') != 0){
-    throw new Exception('Content type must be: application/json');
-}
+// $contentType = isset($_SERVER["CONTENT_TYPE"]) ? trim($_SERVER["CONTENT_TYPE"]) : '';
+// if(strcasecmp($contentType, 'application/json') != 0){
+//     throw new Exception('Content type must be: application/json');
+// }
  
 //Receive the RAW post data.
 $content = trim(file_get_contents("php://input"));
