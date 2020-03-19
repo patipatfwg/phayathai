b4b2d41e5e103b36655b70fed70a258630846bd0
patipatfwg <patipat.fwg@gmail.com>
2020-03-19 23:39:30 +0700

    2339

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <head>
 <meta charset="UTF-8">
 <link href="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
-<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>    
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.4.1.slim.js"></script>  
+<!-- <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>     -->
+<!-- <script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script> -->
 <style>
     .ds-btn li{ list-style:none; float:left; padding:10px; }
     .ds-btn li a span{padding-left:15px;padding-right:5px;width:100%;display:inline-block; text-align:left;}
